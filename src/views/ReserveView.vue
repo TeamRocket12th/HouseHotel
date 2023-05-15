5efb7d41db9a52415d0fddc27c04b7e2924eee43
benson <benson95217@gmail.com>
2023-05-15 20:05:25 +0800

    update: 輪播元件取得API做渲染

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,12 +1,48 @@
 <template>
-  <div>
-    <div class="left">
-      <RoomCarousel />
-    </div>
+  <div class="flex col justify-between">
+    <RoomCarousel :src="roomInfo" />
   </div>
 </template>
 <script setup>
 import RoomCarousel from '../components/RoomCarousel.vue'
+import { ref, onMounted } from 'vue'
+import { apiGetAllRoomInfos, apiGetRoomInfos } from '@/apis/client.js'
+import router from '@/router'
+////////////////////////////////////////////////////////////////
+const props = defineProps({
+  roomId: {
+    required: true
+  }
+})
+const roomId = ref()
+////////////////////////////////////////////////////////////////
+const roomInfo = ref([])
+
+const getAllRoomInfos = async () => {
+  try {
+    const res = await apiGetAllRoomInfos()
+    roomId.value = res.data.items[0].id
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const getRoomInfos = async () => {
+  try {
+    // const roomId = router.params
+    // console.log(roomId)
+    const res = await apiGetRoomInfos(roomId.value) // props: roomId
+    roomInfo.value = res.data.room[0]
+    console.log(roomInfo.value)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(async () => {
+  await getAllRoomInfos()
+  await getRoomInfos()
+})
 </script>
 
 <style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors:{
-        home:{
-         gray:{
-          50:'#0000000A',
-          100:'#00000029',
-          150:'#6A6A6A',
-          200:'#707070'
-         },
-         green:{
-          50:'#949C7C',
-          100:'#38470B'
-         }
+      colors: {
+        home: {
+          gray: {
+            50: '#0000000A',
+            100: '#00000029',
+            150: '#6A6A6A',
+            200: '#707070'
+          },
+          green: {
+            50: '#949C7C',
+            100: '#38470B'
+          }
         }
-      },
-      fontSize:{
-        '2.5xl':'26px',
-        '5.5xl':'50px'
       }
-
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
