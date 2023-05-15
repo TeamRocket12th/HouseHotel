ace18a8fe78391c8eb9d0f5e65482d608ec86ece
benson <benson95217@gmail.com>
2023-05-15 11:17:43 +0800

    fix: 更改token錯誤

--- src/views/ReserveView.vue
+++ src/views/ReserveView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <div class="left">
+      <RoomCarousel />
+    </div>
+  </div>
 </template>
+<script setup>
+import RoomCarousel from '../components/RoomCarousel.vue'
+</script>
 
 <style></style>
