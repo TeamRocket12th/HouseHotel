efe10ca0b94ea776207602901496d890525f6570
tsaaiiiii <a0909513991@gmail.com>
2023-05-16 12:40:36 +0800

    style: 新增css樣式

--- src/components/RoomCards.vue
+++ src/components/RoomCards.vue
@@ -2,6 +2,7 @@
 import { apiGetAllRoomInfos } from '@/apis/client.js'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
+
 const router = useRouter()
 const data = ref([])
 
@@ -28,12 +29,19 @@ onMounted(() => {
 <template>
   <div class="flex flex-wrap">
     <ul v-for="item in data" :key="item.id" class="m-0 w-[275px] list-none p-0">
-      <li @click="clickCards(item.id)" class="hover:bg-gray-800">
-        <div class="relative z-50">
-          <img :src="item.imageUrl" alt="" class="h-[275px] w-full" />
+      <li @click="clickCards(item.id)">
+        <div
+          class="group absolute h-[275px] w-[275px] cursor-pointer bg-transparent opacity-60 hover:bg-home-green-100"
+        >
+          <span
+            class="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 cursor-pointer text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          >
+            {{ item.name }}
+          </span>
         </div>
+
+        <img :src="item.imageUrl" alt="" class="h-[275px] w-full" />
         <!-- <RouterLink :to="`/reserve/${item.id}`"></RouterLink> -->
-        <span>{{ item.name }}</span>
       </li>
     </ul>
   </div>
