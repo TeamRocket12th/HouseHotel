d01121e1b585ec21237302c3e9c599de9da188d5
tsaaiiiii <a0909513991@gmail.com>
2023-05-16 15:15:00 +0800

    style: 新增首頁樣式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,18 @@ import RoomCards from '@/components/RoomCards.vue'
 </script>
 
 <template>
-  <main>
-    <RoomCards />
+  <main class="bg-slate-500 pt-32">
+    <div class="flex gap-32">
+      <div class="pl-[137px]">
+        <img src="@/assets/images/logo.svg" alt="" class="w-100% max-w-none" />
+      </div>
+      <RoomCards />
+    </div>
+    <div class="pl-[137px] text-xs text-white">
+      <p class="mb-4">好室旅店。HOUSE HOTEL</p>
+      <p class="mb-1.5 font-light">花蓮縣花蓮市國聯一路1號</p>
+      <p class="mb-1.5 font-light">03-8321155</p>
+      <p class="font-light">HOUSE@HOTEL.COM</p>
+    </div>
   </main>
 </template>
