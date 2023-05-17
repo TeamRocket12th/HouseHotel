dd113597eaa36e2efee5c6a24916ff5498a958bb
benson <benson95217@gmail.com>
2023-05-17 13:51:06 +0800

    feat: tailwind 新增h標籤字型大小

---
+++ src/components/CalendarItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- <Calendar /> -->
+    <DatePicker v-model="date" borderless />
+    <DatePicker v-model.range.number="range" :columns="2" expanded />
+    <!-- <template #default="{ togglePopover }">
+      <button
+        class="px-3 py-2 bg-blue-500 text-sm text-white font-semibold rounded-md"
+        @click="togglePopover"
+      >
+        Select date
+      </button>
+    </template> -->
+
+    {{ orderDate }}
+  </div>
+</template>
+<script setup>
+import { ref, computed } from 'vue'
+import { DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
+
+const date = ref(new Date())
+const range = ref({ start: date, end: date })
+const orderDate = computed(() => {
+  let firstDay = new Date(range.value.start)
+  let lastDay = new Date(range.value.end)
+  let totalNight = (lastDay - firstDay) / (3600 * 24 * 1000)
+  console.log(totalNight)
+  return [range.value.start, range.value.end]
+})
+</script>
+<style lang=""></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
@@ -23,5 +26,14 @@ module.exports = {
       tc: ['Noto Sans TC', 'sans-serif']
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        h1: { fontSize: '40px', lineHeight: '55px', fontWeight: '600' },
+        h2: { fontSize: '24px', lineHeight: '33px', fontWeight: 'bold' },
+        h3: { fontSize: '50px', lineHeight: '74px', fontWeight: 'medium' },
+        h4: { fontSize: '16px', lineHeight: '24px', fontWeight: 'bold' }
+      })
+    })
+  ]
 }
